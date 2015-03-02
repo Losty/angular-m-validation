6561a85a50ed983d49c2656ed8ca9f869bb2c250
Alexandr Dascal <Alexandr.Dascal@gmail.com>
2015-03-02 23:21:20 +0200

    validation

--- src/directives/mValidationDirective.js
+++ src/directives/mValidationDirective.js
@@ -8,15 +8,14 @@
     angular.module('angular-m-validation.directives')
         .directive('mValidation', ['$injector', function ($injector) {
             var $scope,
-                mValidationProvider,
-                $this = this;
+                mValidationProvider;
 
             var setup = function (injector) {
                 $scope = injector.get('$scope');
                 mValidationProvider = injector.get('mValidationProvider');
             };
 
-            $this.parseSchema = function (schemaName) {
+            var parseSchema = function (schemaName) {
                 var schema = $scope[schemaName] || mValidationProvider.getSchema(schemaName);
 
                 if (!schema) {
@@ -25,27 +24,25 @@
                 return schema;
             };
 
-            $this.parseSchemaElements = function (schema, validateOn) {
+            var parseSchemaElements = function (schema, validateOn) {
                 var schemaElements = [];
+                var schemaGlobals = schema.globals || schema.__globals;
                 for (var prop in schema) {
                     if (schema.hasOwnProperty(prop)) {
-                        var schemaGlobals = schema.globals || schema.__globals;
-
-                        var schemaElement = angular.copy(schema[prop]);
+                        var schemaElement = schema[prop];
 
                         schemaElement.validateOn = schemaElement.validateOn ||
-                                                    schema.validateOn ||
                                                     schemaGlobals.validateOn ||
                                                     validateOn;
 
-                        if (validateOn === 'change') {
+                        if (schemaElement.validateOn === 'change') {
                             // remove old watcher
                             if (schemaElement.watcher) {
                                 schemaElement.watcher();
                             }
 
-                            var schemaElementName = schemaElement.name || prop;
-                            schemaElement.watcher = $scope.$watch(schemaElementName, function() {
+                            schemaElement.name = schemaElement.name || prop;
+                            schemaElement.watcher = $scope.$watch(schemaElement.name, function() {
                                 // validate
                             });
                         }
@@ -57,26 +54,69 @@
                 return schemaElements;
             };
 
+            var parseFormElements = function(form, schema, validateOn, schemaElements) {
+                var formElements = [];
+                var schemaGlobals = schema.globals || schema.__globals;
+
+                var angularFormElement = angular.element(form);
+                var angularFormElements = angularFormElement.find('input, select, textarea');
+                angular.forEach(angularFormElements, function(element) {
+                    var elementName = element.attr('name');
+
+                    if(angular.isDefined(elementName)) { // only if name attribute is defined, we can make association
+                        var schemaElement = schemaElements.find(function(schemaElement) {
+                            return schemaElement.name === elementName;
+                        });
+
+                        schemaElement.formElement = element;
+                    }
+
+                    var elementValidateOn = element.attr('validateOn') ||
+                                            schemaGlobals.validateOn ||
+                                            validateOn;
+
+                    if(elementValidateOn === 'blur') {
+                        // validate
+                    }
+                });
+
+                return formElements;
+            };
+
+            var parse = function(form, schemaName, validateOn) {
+                var schema = parseSchema(schemaName);
+                var schemaElements = parseSchemaElements(schema, validateOn);
+                var formElements = parseFormElements(form, schema, validateOn, schemaElements);
+
+                if(validateOn === 'submit') {
+                    // validate
+                }
+
+                return {
+                    schema: schema,
+                    schemaElements: schemaElements,
+                    formElements: formElements
+                };
+            };
+
             return {
                 restrict: 'AE',
-                compile: function (element, attrs) {
+                compile: function (form, attrs) {
                     setup($injector);
 
                     if (angular.isUndefined(attrs.schema)) {
                         throw new Error('Schema is not defined');
                     }
 
-                    var schema = $this.parseSchema(attrs.schema);
-                    var schemaElements = $this.parseSchemaElements(schema, attrs.validateOn);
-
                     attrs.$observe('schema', function (schemaName) {
-                        var schema = $this.parseSchema(schemaName);
-                        var schemaElements = $this.parseSchemaElements(schema, attrs.validateOn);
+                        parse(form, schemaName, attrs.validateOn);
                     });
 
                     attrs.$observe('validateOn', function (validateOn) {
-                        var schemaElements = $this.parseSchemaElements(schema, validateOn);
+                        parse(form, attrs.schema, validateOn);
                     });
+
+                    parse(form, attrs.schema, attrs.validateOn);
                 }
             };
         }]);
